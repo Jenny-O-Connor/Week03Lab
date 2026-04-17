513ff99711aba444739a7b129d1d85b71f5d4446
(20112226) Jenny O Connor <20112226@setu.ie>
2026-04-17 13:34:23 +0100

    Completed Add Functions lab

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,10 +2,12 @@
 
 import logger from "../utils/logger.js";
 import playlistStore from "../models/playlist-store.js";
+import { v4 as uuidv4 } from 'uuid';
 
 const dashboard = {
   createView(request, response) {
     logger.info("Dashboard page loading!");
+
     
     const viewData = {
       title: "Playlist App Dashboard",
@@ -16,7 +18,19 @@ const dashboard = {
     
     response.render('dashboard', viewData);
   },
+  addPlaylist(request, response) {
+    const newPlayList = {
+      id: uuidv4(),
+      title: request.body.title,
+      songs: [],
+    };
+    playlistStore.addPlaylist(newPlayList);
+    response.redirect('/dashboard');
+  },
 };
+
+
+
 
 export default dashboard;
 
