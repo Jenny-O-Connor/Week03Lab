ce1000ec6ec3de1f1365bc39fbd797d25202daa6
(20112226) Jenny O Connor <20112226@setu.ie>
2026-04-17 14:38:22 +0100

    Added custom helpers

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -19,18 +19,25 @@ const dashboard = {
     response.render('dashboard', viewData);
   },
   addPlaylist(request, response) {
-    const newPlayList = {
+    const timestamp = new Date();
+    
+    const newPlaylist = {
       id: uuidv4(),
       title: request.body.title,
-      songs: [],
+	    date: timestamp,
+      rating: 
+      songs: []
     };
-    playlistStore.addPlaylist(newPlayList);
+    playlistStore.addPlaylist(newPlaylist);
     response.redirect('/dashboard');
+},
+  deletePlaylist(request, response) {
+    const playlistId = request.params.id;
+    logger.debug(`Deleting Playlist ${playlistId}`);
+    playlistStore.removePlaylist(playlistId);
+    response.redirect("/dashboard");
   },
 };
-
-
-
 
 export default dashboard;
 
